e7f75c0701290c1dd09d5d22ae70b45a9acba3ca
LisaVika <gura30rus@gmail.com>
2019-10-26 14:14:06 +0400

    init

---
+++ js/3.js
@@ -0,0 +1,19 @@
+
+ // document.write('Функция принимает параметр - возраст пользователя. Если число больше 16 – то выводим «добро пожаловать», если меньше – “вы еще молоды”.');
+
+ // var year = 14;
+ // if(year>=16)
+ // {
+
+ // document.write('<br>добро пожаловать');
+
+ // }
+ // else  document.write('<br>вы еще молоды');
+
+ var sqrt = 4;
+ function sqrt(digit)
+ {
+ var result = digit * digit;
+ alert(result);
+ }
+ sqrt(sqrt);
---
+++ Файл.php
@@ -0,0 +1,10 @@
+<?php
+ $Years=$_POST['Возраст'];
+
+
+
+if(isset($_POST['Возраст'])) $Years = $_POST['Возраст'];
+
+
+echo ($Fugura);
+?>
